2ad4b97f577185f24e77897682bab3badc04222e
Winson Hong <winsonhong2001@gmail.com>
2024-06-24 12:22:14 +0800

    Update user.resolver.js

--- backend/resolvers/user.resolver.js
+++ backend/resolvers/user.resolver.js
@@ -6,6 +6,9 @@ const userResolver = {
         users: () => {
             return users;
         },
+        user: (_, {userId}) => {
+            return users.find((user) => user._id === userId);
+        }
     },
 
     Mutation: {},

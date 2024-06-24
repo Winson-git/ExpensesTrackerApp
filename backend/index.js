271e90670906b7ed5d48612f2935ac8f1a165618
Winson Hong <winsonhong2001@gmail.com>
2024-06-24 12:29:25 +0800

    convert - Apollo to express version

--- backend/index.js
+++ backend/index.js
@@ -1,14 +1,46 @@
+// npm install @apollo/server express graphql cors
+import express from 'express';
+import http from 'http';
+import cors from 'cors';
 import { ApolloServer } from "@apollo/server"
-import { startStandaloneServer } from "@apollo/server/standalone"
+import { expressMiddleware } from '@apollo/server/express4';
+import { ApolloServerPluginDrainHttpServer } from '@apollo/server/plugin/drainHttpServer';
 
 import mergeResolvers from "./resolvers/index.js"
 import mergedTypeDefs from "./typeDefs/index.js"
+
+// Required logic for integrating with Express
+const app = express();
+// Our httpServer handles incoming requests to our Express app.
+// Below, we tell Apollo Server to "drain" this httpServer,
+// enabling our servers to shut down gracefully.
+const httpServer = http.createServer(app);
  
+// Same ApolloServer initialization as before, plus the drain plugin
+// for our httpServer.
 const server = new ApolloServer({
   typeDefs: mergedTypeDefs,
   resolvers: mergeResolvers,
+  plugins: [ApolloServerPluginDrainHttpServer({ httpServer })],
 })
  
-const { url } = await startStandaloneServer(server)
- 
-console.log(`🚀 Server ready at ${url}`)
+// Ensure we wait for our server to start
+await server.start();
+
+// Set up our Express middleware to handle CORS, body parsing,
+// and our expressMiddleware function.
+app.use(
+  '/',
+  cors(),
+  express.json(),
+  // expressMiddleware accepts the same arguments:
+  // an Apollo Server instance and optional configuration options
+  expressMiddleware(server, {
+    context: async ({ req }) => ({ req }),
+  }),
+);
+
+// Modified server startup
+await new Promise((resolve) => httpServer.listen({ port: 4000 }, resolve));
+
+console.log(`🚀 Server ready at http://localhost:4000/`);

d7c7db29a482b4e7c2f90c4ae5df94f4436cd7f9
Winson Hong <winsonhong2001@gmail.com>
2024-06-24 15:28:21 +0800

    Update - Mutation Transaction resolver

--- backend/resolvers/transaction.resolver.js
+++ backend/resolvers/transaction.resolver.js
@@ -27,7 +27,42 @@ const transactionResolver = {
 		},
 
     },
-    Mutation: {},
+    
+    Mutation: {
+        createTransaction: async (_, { input }, context) => {
+			try {
+				const newTransaction = new Transaction({
+					...input, // get all input 
+					userId: context.getUser()._id,
+				});
+				await newTransaction.save();
+				return newTransaction;
+			} catch (err) {
+				console.error("Error creating transaction:", err);
+				throw new Error("Error creating transaction");
+			}
+		},
+		updateTransaction: async (_, { input }) => {
+			try {
+				const updatedTransaction = await Transaction.findByIdAndUpdate(input.transactionId, input, {
+					new: true,
+				});
+				return updatedTransaction;
+			} catch (err) {
+				console.error("Error updating transaction:", err);
+				throw new Error("Error updating transaction");
+			}
+		},
+		deleteTransaction: async (_, { transactionId }) => {
+			try {
+				const deletedTransaction = await Transaction.findByIdAndDelete(transactionId);
+				return deletedTransaction;
+			} catch (err) {
+				console.error("Error deleting transaction:", err);
+				throw new Error("Error deleting transaction");
+			}
+		},
+    },
 };
 
 export default transactionResolver;

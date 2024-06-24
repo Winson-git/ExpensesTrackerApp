1bf6b562d1eb85208ea07e3697d7467ca9cc1d1c
Winson Hong <winsonhong2001@gmail.com>
2024-06-24 15:24:50 +0800

    Update - Query Transaction resolver

--- backend/resolvers/transaction.resolver.js
+++ backend/resolvers/transaction.resolver.js
@@ -1,7 +1,32 @@
 import { Query } from "mongoose";
+import Transaction from "../models/transaction.model.js";
+import User from "../models/user.model.js";
 
 const transactionResolver = {
-    Query: {},
+    Query: {
+        transactions: async (_, __, context) => {
+			try {
+				if (!context.getUser()) throw new Error("Unauthorized");
+				const userId = await context.getUser()._id;
+
+				const transactions = await Transaction.find({ userId });
+				return transactions;
+			} catch (err) {
+				console.error("Error getting transactions:", err);
+				throw new Error("Error getting transactions");
+			}
+		},
+        transaction: async (_, { transactionId }) => {
+			try {
+				const transaction = await Transaction.findById(transactionId);
+				return transaction;
+			} catch (err) {
+				console.error("Error getting transaction:", err);
+				throw new Error("Error getting transaction");
+			}
+		},
+
+    },
     Mutation: {},
 };
 

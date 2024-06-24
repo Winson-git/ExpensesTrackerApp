c24adc12b8226af795711a351689ab55ad4b4a3f
Winson Hong <winsonhong2001@gmail.com>
2024-06-24 15:20:43 +0800

    Update - Query

--- backend/resolvers/user.resolver.js
+++ backend/resolvers/user.resolver.js
@@ -79,12 +79,24 @@ const userResolver = {
     },
 
     Query: {
-        users: (_, __, { req, res }) => {
-            return users;
+        authUser: async(_, __, context) => {
+            try{
+                const user = await context.getUser();
+				return user;
+            } catch (err){
+                console.error("Error in authUser:", err);
+				throw new Error(err.message || "Internal server error");
+            }
         },
-        user: (_, {userId}) => {
-            return users.find((user) => user._id === userId);
-        }
+        user: async (_, { userId }) => {
+			try {
+				const user = await User.findById(userId);
+				return user;
+			} catch (err) {
+				console.error("Error in user query:", err);
+				throw new Error(err.message || "Error getting user");
+			}
+		},
     },
 
 };

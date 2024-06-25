3b210cd8c3a99d1893e97f948736c70e9555cd7f
Winson Hong <winsonhong2001@gmail.com>
2024-06-25 12:19:45 +0800

    add - Routes

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,6 +1,15 @@
+import { Route, Routes } from "react-router-dom"
+
 function App() {
   return (
     <>
+      <Routes>
+        {/* <Route path='/' element={<HomePage />} /> */}
+				{/* <Route path='/login' element={<LoginPage />} /> */}
+				<Route path='/signup' element={<SignUpPage />} />
+				{/* <Route path='/transaction/:id' element={<TransactionPage />} /> */}
+				{/* <Route path='*' element={<NotFound />} /> */}
+      </Routes>
     </>
   )
 }
--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -2,9 +2,15 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
 import './index.css'
+import { BrowserRouter } from 'react-router-dom';
+import GridBackground from './components/ui/GribBackground.jsx';
 
-ReactDOM.createRoot(document.getElementById('root')).render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-)
+ReactDOM.createRoot(document.getElementById("root")).render(
+	<React.StrictMode>
+		<BrowserRouter>
+			<GridBackground>
+				<App />
+			</GridBackground>
+		</BrowserRouter>
+	</React.StrictMode>
+);

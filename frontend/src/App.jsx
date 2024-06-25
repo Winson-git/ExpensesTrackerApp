2eaede3574e306313ac7634177f55c178783d7de
Winson Hong <winsonhong2001@gmail.com>
2024-06-25 12:30:00 +0800

    add - Files & Header

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,14 +1,22 @@
 import { Route, Routes } from "react-router-dom"
+import HomePage from "./pages/HomePage"
+import LoginPage from "./pages/LoginPage"
+import SignUpPage from "./pages/SignUpPage"
+import TransactionPage from "./pages/Transaction"
+import NotFoundPage from "./pages/NotFoundPage"
+import Header from "./components/ui/Header"
 
 function App() {
+  const authUser = true;
   return (
     <>
+      { authUser && <Header /> }
       <Routes>
-        {/* <Route path='/' element={<HomePage />} /> */}
-				{/* <Route path='/login' element={<LoginPage />} /> */}
+        <Route path='/' element={<HomePage />} />
+				<Route path='/login' element={<LoginPage />} />
 				<Route path='/signup' element={<SignUpPage />} />
-				{/* <Route path='/transaction/:id' element={<TransactionPage />} /> */}
-				{/* <Route path='*' element={<NotFound />} /> */}
+				<Route path='/transaction/:id' element={<TransactionPage />} />
+				<Route path='*' element={<NotFoundPage />} />
       </Routes>
     </>
   )

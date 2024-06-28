db55bba52f22cf97dd35ade7bec80ea3e801dc6a
Winson Hong <winsonhong2001@gmail.com>
2024-06-28 16:34:14 +0800

    add - Delete function

--- frontend/src/components/Card.jsx
+++ frontend/src/components/Card.jsx
@@ -8,6 +8,7 @@ import { Link } from "react-router-dom";
 import { formatDate } from "../utils/formatDate";
 import toast from "react-hot-toast";
 import { useMutation } from "@apollo/client";
+import { DELETE_TRANSACTION } from "../graphql/mutations/transaction.mutation"
 
 
 const categoryColorMap = {
@@ -22,6 +23,10 @@ const Card = ({ transaction }) => {
 
 	const cardClass = categoryColorMap[category]; // will render this function first 
 
+	const [deleteTransaction, { loading }] = useMutation(DELETE_TRANSACTION, {
+		refetchQueries: ["GetTransactions" ],
+	});
+
 	// Capitalize the first letter of the description
 	description = description[0]?.toUpperCase() + description.slice(1);
 	category = category[0]?.toUpperCase() + category.slice(1);
@@ -29,13 +34,24 @@ const Card = ({ transaction }) => {
 
 	const formattedDate = formatDate(date);
 
+	const handleDelete = async () => {
+		try {
+			await deleteTransaction({ variables: { transactionId: transaction._id } });
+			toast.success("Transaction deleted successfully");
+		} catch (error) {
+			console.error("Error deleting transaction:", error);
+			toast.error(error.message);
+		}
+	}
+
 	return (
 		<div className={`rounded-md p-4 bg-gradient-to-br ${cardClass}`}>
 			<div className='flex flex-col gap-3'>
 				<div className='flex flex-row items-center justify-between'>
 					<h2 className='text-lg font-bold text-white'>{category}</h2>
 					<div className='flex items-center gap-2'>
-						<FaTrash className={"cursor-pointer"} onClick={ handleDelete }/>
+						{!loading && <FaTrash className={"cursor-pointer"} onClick={handleDelete} />}
+						{loading && <div className='w-6 h-6 border-t-2 border-b-2 rounded-full animate-spin'></div>}
 						<Link to={`/transaction/123`}>
 							<HiPencilAlt className='cursor-pointer' size={20} />
 						</Link>

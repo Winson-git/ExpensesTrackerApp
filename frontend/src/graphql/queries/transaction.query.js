be6b505287f01f27cf33b16e2e1190392ae2f4cb
Winson Hong <winsonhong2001@gmail.com>
2024-06-28 16:29:04 +0800

    Update - Card with Backend

---
+++ frontend/src/graphql/queries/transaction.query.js
@@ -0,0 +1,16 @@
+import { gql } from "@apollo/client";
+
+export const GET_TRANSACTIONS = gql`
+	query GetTransactions {
+		transactions {
+			_id
+			description
+			paymentType
+			category
+			amount
+			location
+			date
+		}
+	}
+`;
+

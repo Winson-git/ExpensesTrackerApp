07380d039e130880a8c73a0435f5b32861ff1337
Winson Hong <winsonhong2001@gmail.com>
2024-07-01 23:54:45 +0800

    add - Card Profile

--- frontend/src/components/Card.jsx
+++ frontend/src/components/Card.jsx
@@ -18,7 +18,7 @@ const categoryColorMap = {
 	// Add more categories and corresponding color classes as needed
 };
 
-const Card = ({ transaction }) => {
+const Card = ({ transaction, authUser }) => {
 	let { category, amount, location, date, paymentType, description } = transaction; 
 
 	const cardClass = categoryColorMap[category]; // will render this function first 
@@ -76,7 +76,7 @@ const Card = ({ transaction }) => {
 				<div className='flex justify-between items-center'>
 					<p className='text-xs text-black font-bold'>{formattedDate}</p>
 					<img
-						src={"https://tecdn.b-cdn.net/img/new/avatars/2.webp"}
+						src={authUser?.profilePicture}
 						className='h-8 w-8 border rounded-full'
 						alt=''
 					/>

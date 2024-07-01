e9f6fd369140f894d5d0cf551523b2f80850638a
Winson Hong <winsonhong2001@gmail.com>
2024-07-01 19:49:14 +0800

    Update - Transaction Mutation

--- frontend/src/pages/Transaction.jsx
+++ frontend/src/pages/Transaction.jsx
@@ -2,13 +2,19 @@ import { useMutation, useQuery } from "@apollo/client";
 import { useEffect, useState } from "react";
 import { useParams } from "react-router-dom";
 import { GET_TRANSACTION } from "../graphql/queries/transaction.query";
+import { UPDATE_TRANSACTION } from "../graphql/mutations/transaction.mutation";
+import toast from "react-hot-toast";
+import TransactionFormSkeleton from "../components/skeletons/TransactionFormSkeleton";
 
 const TransactionPage = () => {
 	const { id } = useParams();
 	const { loading, data } = useQuery(GET_TRANSACTION, {
 		variables: { id: id },
 	});
-	console.log("Transaction Data:", data);
+
+	const [updateTransaction, { loading: loadingUpdate }] = useMutation(UPDATE_TRANSACTION, {
+		
+	});
 
 	const [formData, setFormData] = useState({
 		description: data?.transaction?.description || "",
@@ -21,7 +27,21 @@ const TransactionPage = () => {
 
 	const handleSubmit = async (e) => {
 		e.preventDefault();
-		console.log("formData", formData);
+		const amount = parseFloat(formData.amount); // convert to int ; default is string due to input
+		try {
+			await updateTransaction({
+				variables:{
+					input:{
+						...formData,
+						amount,
+						transactionId: id,
+					}
+				}
+			})
+			toast.success("Transaction updated successfully");
+		} catch (error) {
+			toast.error(error.message);
+		}
 	};
 	const handleInputChange = (e) => {
 		const { name, value } = e.target;
@@ -44,7 +64,7 @@ const TransactionPage = () => {
 		}
 	}, [data]);
 
-	// if (loading) return <TransactionFormSkeleton />;
+	if (loading) return <TransactionFormSkeleton />;
 
 	return (
 		<div className='h-screen max-w-4xl mx-auto flex flex-col items-center'>
@@ -198,8 +218,9 @@ const TransactionPage = () => {
 					className='text-white font-bold w-full rounded px-4 py-2 bg-gradient-to-br
           from-pink-500 to-pink-500 hover:from-pink-600 hover:to-pink-600'
 					type='submit'
+					disabled={loadingUpdate}
 				>
-					Update Transaction
+					{ loadingUpdate ? 'Updating...' : 'Update Transaction'}
 				</button>
 			</form>
 		</div>
